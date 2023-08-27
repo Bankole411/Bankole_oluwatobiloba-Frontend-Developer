94e1e26a923be6d12d8d605d10040dcdffb69f85
Bankole Oluwatobiloba <66980300+Bankole411@users.noreply.github.com>
2023-08-27 01:21:49 +0100

    Adding Pop-ups to Rockets.js

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,19 +1,20 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
-import Loading from "../components/Loading"
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Loading from "../components/Loading";
 
 export default function Rockets() {
-  const [rockets, setRockets] = useState(null)
+  const [rockets, setRockets] = useState(null);
+  const [selectedRocket, setSelectedRocket] = useState(null);
 
   useEffect(() => {
     const fetchRockets = async () => {
-      const res = await fetch(`https://api.spacexdata.com/v4/rockets`)
-      const data = await res.json()
-      setRockets(data)
-    }
+      const res = await fetch(`https://api.spacexdata.com/v4/rockets`);
+      const data = await res.json();
+      setRockets(data);
+    };
 
-    fetchRockets()
-  }, [])
+    fetchRockets();
+  }, []);
 
   return (
     <>
@@ -25,8 +26,12 @@ export default function Rockets() {
             <h1 className="heading ml-5">Rockets</h1>
 
             <div className="max-width grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
-              {rockets.map(({ id, name, description, flickr_images }) => (
-                <Link to={`/rockets/${id}`} key={id}>
+              {rockets.map(({ id, name, description, flickr_images, wikipedia }) => (
+                <div
+                  key={id}
+                  onClick={() => setSelectedRocket(id)}
+                  className="rocket-card"
+                >
                   <article className="articles">
                     <img
                       src={flickr_images[0]}
@@ -41,17 +46,38 @@ export default function Rockets() {
                         0,
                         100
                       )}...`}</p>
-                      <Link to={`/rockets/${id}`} className="btn">
-                        Learn more
+                      <Link to="#" className="btn">
+                        Details
                       </Link>
                     </div>
                   </article>
-                </Link>
+                </div>
               ))}
             </div>
           </div>
+          {selectedRocket && (
+            <div className="popup-overlay">
+              <div className="popup-content">
+                <h2 className="popup-heading">
+                  {rockets.find((rocket) => rocket.id === selectedRocket)?.name}
+                </h2>
+                <p className="popup-description">
+                  {rockets.find((rocket) => rocket.id === selectedRocket)?.description}
+                </p>
+                <a
+                  href={rockets.find((rocket) => rocket.id === selectedRocket)?.wikipedia}
+                  className="popup-link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {rockets.find((rocket) => rocket.id === selectedRocket)?.wikipedia}
+                </a>
+                <button className="popup-close" onClick={() => setSelectedRocket(null)}>Close</button>
+              </div>
+            </div>
+          )}
         </section>
       )}
     </>
-  )
+  );
 }
